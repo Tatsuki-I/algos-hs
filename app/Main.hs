1cfec4575defc1dddfc85a1a133313a42ff11a6c
Tatsuki <tatsuki@konokaze.jp>
2017-07-14 11:50:02 +0900

    add isort

--- app/Main.hs
+++ app/Main.hs
@@ -1,6 +1,7 @@
 module Main where
 
 import Sort.QuickSort
+import Sort.InsertSort
 
 main :: IO ()
-main = print "hello"
+main = print $ insertSort [6,5,4,3,2,1]
---
+++ src/Sort/InsertSort.hs
@@ -0,0 +1,7 @@
+module Sort.InsertSort where
+
+import Data.List
+
+insertSort :: Ord a => [a] -> [a]
+insertSort [] = []
+insertSort (x : xs) = insert x $ insertSort xs
