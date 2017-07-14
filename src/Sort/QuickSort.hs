fed6e64a873b6cb1928b9216f06ad5a027912b64
Tatsuki <tatsuki@konokaze.jp>
2017-07-14 20:48:14 +0900

    add msort

--- src/Sort/QuickSort.hs
+++ src/Sort/QuickSort.hs
@@ -4,6 +4,6 @@ import Data.List
 
 quickSort :: Ord a => [a] -> [a]
 quickSort []       = []
-quickSort (x : xs) = quickSort lesser ++ [x] ++ quickSort greater
+quickSort (x : xs) = quickSort lesser ++ x : quickSort greater
     where
         (lesser, greater) = partition (<x) xs
