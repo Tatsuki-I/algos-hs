8d6ca058035b93bbe5f864d62f5b0405c5bf0093
Tatsuki <tatsuki@konokaze.jp>
2017-07-22 00:44:27 +0900

    add another selection sort

--- src/Sort/SelectionSort.hs
+++ src/Sort/SelectionSort.hs
@@ -1,12 +1,18 @@
 module Sort.SelectionSort where
 
 import           Data.List
+import Control.Monad
+import Control.Monad.Fix
 
 selectionSort :: Ord a => [a] -> [a]
-selectionSort []   = []
+--selectionSort []   = []
+selectionSort = fix (((:) <$>  minimum <*>) . (. (delete =<< minimum)))
+--selectionSort = fix (liftM2 (:) minimum . (. (delete =<< minimum)))
+{-
 selectionSort list = minX : selectionSort (delete minX list)
     where
         minX = minimum list
+        -}
 
 separateMinimum :: Ord a => [a] -> Maybe (a, [a])
 separateMinimum []   = Nothing
