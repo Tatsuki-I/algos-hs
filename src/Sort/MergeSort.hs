8afb325f70aa0a88b463fcbd0adab17308658a0a
Tatsuki <tatsuki@konokaze.jp>
2017-07-25 13:45:31 +0900

    fix indents

--- src/Sort/MergeSort.hs
+++ src/Sort/MergeSort.hs
@@ -3,11 +3,10 @@ module Sort.MergeSort where
 import           Data.List
 
 merge :: Ord a => [a] -> [a] -> [a]
-merge [] y = y
-merge x [] = x
-merge (x : xs) (y : ys)
-        | x <= y    = x : merge xs (y : ys)
-        | otherwise = y : merge (x : xs) ys
+merge []     y                  = y
+merge x      []                 = x
+merge (x:xs) (y:ys) | x <= y    = x: merge xs     (y:ys)
+                    | otherwise = y: merge (x:xs) ys
 
 halve :: [a] -> ([a], [a])
 halve list = (take lsLength list, drop lsLength list)
@@ -15,8 +14,8 @@ halve list = (take lsLength list, drop lsLength list)
         lsLength = div (length list) 2
 
 mergeSort :: Ord a => [a] -> [a]
-mergeSort []   = []
-mergeSort [x]  = [x]
-mergeSort list = merge (mergeSort $ fst hlist) (mergeSort $ snd hlist)
+mergeSort []  = []
+mergeSort [x] = [x]
+mergeSort xs  = merge (mergeSort $ fst hlist) (mergeSort $ snd hlist)
     where
-        hlist = halve list
+        hlist = halve xs
--- src/Sort/QuickSort.hs
+++ src/Sort/QuickSort.hs
@@ -3,7 +3,7 @@ module Sort.QuickSort where
 import           Data.List
 
 quickSort :: Ord a => [a] -> [a]
-quickSort []       = []
-quickSort (x : xs) = quickSort lesser ++ x : quickSort greater
+quickSort []     = []
+quickSort (x:xs) = quickSort lesser ++ x: quickSort greater
     where
         (lesser, greater) = partition (<x) xs
