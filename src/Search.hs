4406cb053287386673c068edf800cd01fb96affd
Tatsuki <tatsuki@konokaze.jp>
2017-07-25 13:39:14 +0900

    fix indents

--- src/Search.hs
+++ src/Search.hs
@@ -3,17 +3,32 @@ module Search where
 import Sort.QuickSort
 
 sequentialSearch :: Eq a => [a] -> a -> Bool
-sequentialSearch [] _ = False
-sequentialSearch (x : xs) target
-        | x == target = True
-        | otherwise   = sequentialSearch xs target
+sequentialSearch []     _             = False
+sequentialSearch (x:xs) t | x == t    = True
+                          | otherwise = sequentialSearch xs t
+
+{-
+sequentialSearchMaybe :: Eq a => [a] -> a -> Maybe Int
+sequentialSearchMaybe [] _ = Nothing
+sequentialSearchMaybe list@(x : xs) target
+        | x == target = Just (length xs)
+        | otherwise   = sequentialSearchMaybe xs target
+        -}
+
+sequentialSearchMaybe :: Eq a => [a] -> a -> Maybe Int
+sequentialSearchMaybe xs t = sSearchMaybe' xs t 0
+    where
+        sSearchMaybe' :: Eq a => [a] -> a -> Int -> Maybe Int
+        sSearchMaybe' []     _ _             = Nothing
+        sSearchMaybe' (x:xs) t c | x == t    = Just c
+                                 | otherwise = sSearchMaybe' 
+                                         xs t (c + 1)
 
 isSorted :: Ord a => [a] -> Bool
-isSorted []  = True
-isSorted [_] = True
-isSorted (x1 : x2 : xs)
-        | x1 <= x2  = isSorted (x2 : xs)
-        | otherwise = False
+isSorted []                     = True
+isSorted [_]                    = True
+isSorted (x1:x2:xs) | x1 <= x2  = isSorted (x2:xs)
+                    | otherwise = False
 
 data SearchError = Unsorted deriving (Show)
 
@@ -21,33 +36,30 @@ showError :: SearchError -> String
 showError Unsorted = "This list is unsorted."
 
 binarySearch :: Ord a => [a] -> a -> Either SearchError Bool
-binarySearch [] _             = Right False
-binarySearch list target
-        | not $ isSorted list = Left $ Unsorted
-        | midVal == target    = Right True
-        | midVal < target     = binarySearch (drop (mid + 1) list) target
-        | midVal > target     = binarySearch (take mid list) target
-            where
-                mid    = div (length list) 2
-                midVal = list !! mid
+binarySearch [] _                     = Right  False
+binarySearch xs t | not $ isSorted xs = Left $ Unsorted
+                  | midVal == t       = Right  True
+                  | midVal <  t       = binarySearch (drop (mid + 1) xs) t
+                  | midVal >  t       = binarySearch (take mid xs)       t
+                      where
+                          mid    = div (length xs) 2
+                          midVal = xs !! mid
 
 binarySearch' :: Ord a => [a] -> a -> Bool
-binarySearch' [] _            = False
-binarySearch' list target
-        | midVal == target    = True
-        | midVal < target     = binarySearch' (drop (mid + 1) list) target
-        | midVal > target     = binarySearch' (take mid list) target
-            where
-                mid    = div (length list) 2
-                midVal = list !! mid
+binarySearch' [] _               = False
+binarySearch' xs t | midVal == t = True
+                   | midVal <  t = binarySearch' (drop (mid + 1) xs) t
+                   | midVal >  t = binarySearch' (take mid xs)       t
+                       where
+                           mid    = div (length xs) 2
+                           midVal = xs !! mid
 
 binarySearch'' :: Ord a => [a] -> a -> Bool
-binarySearch'' [] _           = False
-binarySearch'' list target
-        | not $ isSorted list = binarySearch'' (quickSort list) target
-        | midVal == target    = True
-        | midVal < target     = binarySearch'' (drop (mid + 1) list) target
-        | midVal > target     = binarySearch'' (take mid list) target
-            where
-                mid    = div (length list) 2
-                midVal = list !! mid
+binarySearch'' [] _                     = False
+binarySearch'' xs t | not $ isSorted xs = binarySearch'' (quickSort xs)      t
+                    | midVal == t       = True
+                    | midVal <  t       = binarySearch'' (drop (mid + 1) xs) t
+                    | midVal >  t       = binarySearch'' (take mid xs)       t
+                        where
+                            mid    = div (length xs) 2
+                            midVal = xs !! mid
